bb491ab56b751c1135c90e6dc2acbc16b531b244
Anastasia Tsilepi <a.tsilepi@dataverse.gr>
2021-04-16 15:55:01 +0300

    restore deleted code

--- src/components/AddEvent.jsx
+++ src/components/AddEvent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyledButton, StyledBackdrop, PaperModal } from "../styles";
 import { Typography, Modal, Grid, TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
--- src/components/CalendarView.jsx
+++ src/components/CalendarView.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import { setDate } from "../redux/actions/index";
+import { useDispatch } from "react-redux";
 
 const CalendarView = () => {
   const [value, onChange] = useState(new Date());
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setDate(value));
+  }, [value]);
 
   return (
     <div>
